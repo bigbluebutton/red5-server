94296a60d621719cfaa183d59208ff75f22077c7
Richard Alam <ritzalam@gmail.com>
2014-04-07 16:56:51 +0000

    - scripts for building red5

---
+++ build-red5.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+
+RED5_SRC=$(pwd)
+RED5_VER=1.0.2-M1
+RED5_DIR=red5-server-${RED5_VER}
+
+echo "Preparing build"
+mvn clean
+
+echo "mvn bootstrap install"
+mvn -Dmaven.test.skip=true -Dclassifier=bootstrap install
+echo "mvn dependencies"
+mvn dependency:copy-dependencies
+echo "mvn package"
+mvn -Dmaven.test.skip=true -Dmaven.buildNumber.doUpdate=false -Dclassifier=bootstrap package
+
+cd $RED5_SRC/target
+echo "Extractring red5"
+tar zxvf red5-server-${RED5_VER}-server.tar.gz
+
+cp $RED5_SRC/extras/commons-fileupload-1.2.2.jar $RED5_DIR/lib
+cp $RED5_SRC/extras/commons-io-2.0.jar $RED5_DIR/lib
+
+echo "Done."
+cd $RED5_SRC
+
